1fb8d3c2d65fc51fabd2d986160600b8488a9695
Isabelle Cristina <110576073+isabellecristinav@users.noreply.github.com>
2023-05-03 22:15:09 -0300

    atualização do site

--- novo-usuario.php
+++ novo-usuario.php
@@ -8,5 +8,14 @@ include "menu.php";
             <h1>Cadastrar novo usuário</h1>
         </div>
     </div>
-    
+    <div class="row">
+        <div class="col">
+            <form method="post" action="salvar-usuario.php">
+                <input name="nome" required placeholder="Nome"><br>
+                <input type="email" name="email" required placeholder="Email"><br>
+                <input type="password" name="senha" required placeholder="Senha"><br>
+                <button type="submit" class="btn btn-success">Salvar Usuário</button>
+            </form>
+        </div>
+    </div>
 </div>
